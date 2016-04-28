d786480b0b94eb448ac6facd9395bb8457b99f39
Emanuele <emanuele.palazzetti@gmail.com>
2016-04-28 15:23:51 +0200

    added WhiteNoise to serve static files (CloudFront origin)

--- django-dispensa/dispensa/wsgi.py
+++ django-dispensa/dispensa/wsgi.py
@@ -1,4 +1,10 @@
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 
+from whitenoise.django import DjangoWhiteNoise
 
-application = get_wsgi_application()
+
+# WhiteNoise is used to serve Django statics; to improve
+# the Python application performance, configure a proper
+# CDN such as CloudFront
+application = DjangoWhiteNoise(get_wsgi_application())
