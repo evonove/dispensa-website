0c030ca8d1efdc13dd928ad3b0f165b1fa7054b5
Alessio Rapini <alessio.rapini@yahoo.it>
2016-05-02 23:08:41 +0200

    improve path.js code

--- django-dispensa/wheelie/client/js/path.js
+++ django-dispensa/wheelie/client/js/path.js
@@ -1,24 +1,28 @@
-var path = document.getElementById('menu-path-line');
+(function() {
+    var path = document.getElementById('menu-path-line');
+    var menu = document.querySelector('.menu');
 
-var pathLength = path.getTotalLength();
-
-var menu = document.querySelector('.menu');
-var menuHeight = menu.offsetHeight;
-
-var referenceTop = menu.offsetTop;
-
-path.style.strokeDashoffset = pathLength;
-
-window.addEventListener("scroll", function(e) {
-
-    var scrollPercentage = (document.body.scrollTop - referenceTop) / ((menuHeight - document.documentElement.clientHeight) + 450);
-    var drawLength = (pathLength * scrollPercentage);
-
-    path.style.strokeDashoffset = pathLength - drawLength;
-
-    if (scrollPercentage >= 0.99) {
-        path.style.strokeDasharray = "none";
-    } else {
-        path.style.strokeDasharray = pathLength + ' ' + pathLength;
+    if (!path || !menu) {
+        return;
     }
-});
+
+    var pathLength = path.getTotalLength();
+    var menuHeight = menu.offsetHeight;
+    var referenceTop = menu.offsetTop;
+
+    path.style.strokeDashoffset = pathLength;
+
+    window.addEventListener("scroll", function() {
+
+        var scrollPercentage = (document.body.scrollTop - referenceTop) / ((menuHeight - document.documentElement.clientHeight) + 450);
+        var drawLength = (pathLength * scrollPercentage);
+
+        path.style.strokeDashoffset = pathLength - drawLength;
+
+        if (scrollPercentage >= 0.99) {
+            path.style.strokeDasharray = "none";
+        } else {
+            path.style.strokeDasharray = pathLength + ' ' + pathLength;
+        }
+    });
+})()
