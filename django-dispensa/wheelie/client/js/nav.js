5d74b2069d718eedcdecab24eb06c62b26c7183c
Alessio Rapini <alessio.rapini@yahoo.it>
2016-05-06 01:38:01 +0200

    extend mobile nav behavior to the sticky header

--- django-dispensa/wheelie/client/js/nav.js
+++ django-dispensa/wheelie/client/js/nav.js
@@ -1,12 +1,31 @@
 (function() {
-    var mobileOpen = document.querySelector('.mobile-header-navigation-icon');
-    var mobileNavigation = document.querySelector('.mobile-header-navigation');
-    var mobileNavigationNav = document.querySelector('.mobile-header-navigation-nav');
+    var mobileOpen = document.querySelectorAll('#mobile-header-navigation-icon');
+    var mobileNavigation = document.querySelector('#mobile-header-navigation');
+    var mobileNavigationNav = document.querySelector('#mobile-header-navigation-nav');
 
-    mobileOpen.addEventListener('click', openNav);
+    var stickyOpen = document.querySelectorAll('.header-navigation-icon.mobile-header-navigation-icon.sticky-header-navigation-icon');
+    var stickyNavigation = document.querySelector('.header-navigation.mobile-header-navigation.sticky-header-navigation');
+    var stickyNavigationNav = document.querySelector('.header-navigation-nav.mobile-header-navigation-nav.sticky-header-navigation-nav');
 
-    function openNav () {
+    console.log('mobileOpen', mobileOpen);
+    console.log('stickyOpen', stickyOpen);
+
+
+    for (var i = 0; i < mobileOpen.length; i++) {
+        mobileOpen[i].addEventListener('click', openNavMobile);
+    }
+
+    for (var i = 0; i < stickyOpen.length; i++) {
+        stickyOpen[i].addEventListener('click', openNavSticky);
+    }
+
+    function openNavMobile () {
         mobileNavigation.classList.toggle('mobile-header-navigation-open');
         mobileNavigationNav.classList.toggle('mobile-header-navigation-nav-open');
+    }
+
+    function openNavSticky () {
+        stickyNavigation.classList.toggle('sticky-header-navigation-open');
+        stickyNavigationNav.classList.toggle('sticky-header-navigation-nav-open');
     }
 })();
