05555524d17aa772cb9cd4c8cdc4c5c3a6189dc3
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-05-07 19:04:44 +0200

    add options management for the homepage

--- django-dispensa/dispensa/settings/base.py
+++ django-dispensa/dispensa/settings/base.py
@@ -53,11 +53,13 @@ INSTALLED_APPS = (
     'wagtail.wagtailsearch',
     'wagtail.wagtailadmin',
     'wagtail.wagtailcore',
+    'wagtail.contrib.settings',
 
     'modelcluster',
     'taggit',
 
     'wheelie',
+    'options',
     'home',
 )
 
@@ -89,6 +91,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'wagtail.contrib.settings.context_processors.settings',
             ],
         },
     },
---
+++ django-dispensa/options/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.utils.translation import ugettext as _
+
+from wagtail.contrib.settings.models import BaseSetting, register_setting
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel
+
+
+@register_setting
+class WebsiteSettings(BaseSetting):
+    vat = models.CharField(max_length=16, help_text=_('Your company VAT with initial state code (i.e.: GB)'))
+    phone = models.CharField(max_length=16, help_text=_('Your company phone number'))
+    email = models.EmailField(max_length=255, help_text=_('Your company email address'))
+
+    facebook = models.URLField(help_text=_('Your Facebook page URL'))
+    instagram = models.URLField(help_text=_('Your Instagram page URL'))
+    pinterest = models.URLField(help_text=_('Your Pinterest page URL'))
+
+    panels = [
+        MultiFieldPanel(
+            [
+                FieldPanel('vat'),
+                FieldPanel('phone'),
+                FieldPanel('email'),
+            ],
+            heading=_('Company information'),
+            classname='collapsible',
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel('facebook'),
+                FieldPanel('instagram'),
+                FieldPanel('pinterest'),
+            ],
+            heading=_('Social media'),
+            classname='collapsible',
+        ),
+    ]
